c1a1f16397e32ffb9852916b4965e26bbf824347
Matyáš Korytar <91247705+MatyasKorytar@users.noreply.github.com>
2022-05-08 12:01:21 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,61 +1,49 @@
+#KLIENT
 radio.set_group(299)
-basic.show_leds("""
-. . . . #
-. . . # .
-# . # . .
-. # . . .
-. . . . .
-""")
-serialnumber = control.device_serial_number()
-ukol = 0
-def on_pin_pressed_p0():
-    if ukol == 0:
-        ukol = 1
-input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
+hlasovani = 0
+if hlasovani == 0:
+    basic.show_leds("""
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    """)
+def on_received_number(receivedNumber):
+    hlasovani = 1
+radio.on_received_number(on_received_number)
+if hlasovani == 1:
+    basic.show_leds("""
+    . . . . .
+    . . . . #
+    . . . # .
+    # . # . .
+    . # . . .
+    """)
+
 volby = ["A", "B", "C", "D", "E", "F", "G"]
 count = [0, 0, 0, 0, 0, 0, 0]
 x = 0
 odeslat = 0
+hlas = 0
+
+#výběr hlasu
 def on_button_pressed_a():
-    if ukol == 0:
         global x
-        basic.show_string(volby[0+x])
-        x += 1
-        if x == len(volby):
-            x = 0
+        global hlas
+        if hlasovani == 1:
+            basic.show_string(volby[0+x])
+            hlas = x
+            x += 1
+            if x == len(volby):
+                x = 0
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+#odeslání hlasu
 def on_button_pressed_b():
-    if ukol == 0:
         global odeslat
-        if odeslat == 0:
-            radio.send_string(volby[0+x])
-        odeslat = 1    
+        if hlasovani == 1:
+            if odeslat == 0:
+                radio.send_string(volby[hlas])
+                odeslat = 1    
 input.on_button_pressed(Button.B, on_button_pressed_b)
-
-def on_received_string(receivedString):
-    global count
-    if ukol == 1:
-        if receivedString == "A":
-            count[0] += 1
-        elif receivedString == "B":
-            count[1] += 1  
-        elif receivedString == "C":
-            count[2] += 1 
-        elif receivedString == "D":
-            count[3] += 1
-        elif receivedString == "E":
-            count[4] += 1 
-        elif receivedString == "F":
-            count[5] += 1
-        elif receivedString == "G":
-            count[6] += 1          
-radio.on_received_string(on_received_string)
-n = 0
-while ukol == 1:
-    basic.show_string(volby[0+n])
-    basic.pause(500)
-    basic.show_number(count[0+n])
-    basic.pause(600)
-    n += 1
-    
--- main.ts
+++ main.ts
@@ -1,28 +1,41 @@
+// KLIENT
 radio.setGroup(299)
-basic.showLeds(`
-. . . . #
-. . . # .
-# . # . .
-. # . . .
-. . . . .
-`)
-let serialnumber = control.deviceSerialNumber()
-let ukol = 0
-input.onPinPressed(TouchPin.P0, function on_pin_pressed_p0() {
-    let ukol: number;
-    if (ukol == 0) {
-        ukol = 1
-    }
-    
+let hlasovani = 0
+if (hlasovani == 0) {
+    basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
+}
+
+radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
+    let hlasovani = 1
 })
+if (hlasovani == 1) {
+    basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . # .
+    # . # . .
+    . # . . .
+    `)
+}
+
 let volby = ["A", "B", "C", "D", "E", "F", "G"]
 let count = [0, 0, 0, 0, 0, 0, 0]
 let x = 0
 let odeslat = 0
+let hlas = 0
+// výběr hlasu
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    if (ukol == 0) {
-        
+    
+    
+    if (hlasovani == 1) {
         basic.showString(volby[0 + x])
+        hlas = x
         x += 1
         if (x == volby.length) {
             x = 0
@@ -31,44 +44,15 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
     }
     
 })
+// odeslání hlasu
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    if (ukol == 0) {
-        
+    
+    if (hlasovani == 1) {
         if (odeslat == 0) {
-            radio.sendString(volby[0 + x])
-        }
-        
-        odeslat = 1
-    }
-    
-})
-radio.onReceivedString(function on_received_string(receivedString: string) {
-    
-    if (ukol == 1) {
-        if (receivedString == "A") {
-            count[0] += 1
-        } else if (receivedString == "B") {
-            count[1] += 1
-        } else if (receivedString == "C") {
-            count[2] += 1
-        } else if (receivedString == "D") {
-            count[3] += 1
-        } else if (receivedString == "E") {
-            count[4] += 1
-        } else if (receivedString == "F") {
-            count[5] += 1
-        } else if (receivedString == "G") {
-            count[6] += 1
+            radio.sendString(volby[hlas])
+            odeslat = 1
         }
         
     }
     
 })
-let n = 0
-while (ukol == 1) {
-    basic.showString(volby[0 + n])
-    basic.pause(500)
-    basic.showNumber(count[0 + n])
-    basic.pause(600)
-    n += 1
-}
